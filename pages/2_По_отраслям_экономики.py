2ff4a6975225380950489421695cd3c491eee90d
Sergey Zinkin <zinkin.s@yandex.ru>
2024-04-08 18:28:08 +0300

    Load data

--- pages/2_По_отраслям_экономики.py
+++ pages/2_По_отраслям_экономики.py
@@ -3,4 +3,13 @@ import numpy as np
 import streamlit as st
 import altair as alt
 
+
+
+
 df = pd.read_csv('data/out.csv')
+df = df.drop(['всего по  экономике', 'производство пищевых продуктов, включая напитки, и табака'], axis=1)
+df['год'] = pd.to_datetime(df['год'], format='%Y')
+inflation = pd.read_csv('data/inflation.csv').sort_values('Год').reset_index()
+
+
+st.dataframe(df)
